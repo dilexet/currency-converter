42d0de56eff91589333310711c5f83876ee102ac
Dilexet <dilexet@gmail.com>
2022-12-01 22:43:03 +0300

    Added redux store and created base project structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+import AppContainer from "./modules/app/containers/AppContainer";
+import {store} from "./modules/shared/store";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App/>
+        <Provider store={store}>
+            <BrowserRouter>
+                <AppContainer/>
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
