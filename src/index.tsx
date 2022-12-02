61273801fc52d7ca0652555dadf87f1e37a18de6
Dilexet <dilexet@gmail.com>
2022-12-02 17:45:00 +0300

    changed index

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
-import AppContainer from "./modules/app/containers/AppContainer";
+import AppContainer from "./modules/app/containers/app-container";
 import {store} from "./modules/shared/store";
 
 const root = ReactDOM.createRoot(
