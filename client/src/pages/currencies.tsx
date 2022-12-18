41caeeb7eda009999df8c9d170503ccde67d4097
Pavarha Maksim <dilexet@gmail.com>
2022-12-18 13:49:33 +0300

    moved code from containers to pages

--- client/src/pages/currencies.tsx
+++ client/src/pages/currencies.tsx
@@ -1,7 +1,61 @@
-import CurrencyListContainer from "../containers/currency-list/currency-list-container";
+import { useCallback, useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
+import { BASE_CURRENCY_KEY } from "../constants/shared/storage-currency.constants";
+import { getCurrencies } from "../services/currency-list-actions";
+import { getLocation } from "../services/get-currency-by-location";
+import CurrencyList from "../components/currency-list/currency-list";
+import Loading from "../components/loading/loading";
 
 const Currencies = () => {
-  return <CurrencyListContainer />;
+  const dispatch = useAppDispatch();
+  const currencies_state = useAppSelector((x) => x.currency);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [baseCurrency, setBaseCurrency] = useState<string>("");
+
+  const changeBaseCurrency = async (newBaseCurrency: string): Promise<void> => {
+    await fetchData(newBaseCurrency);
+    setBaseCurrency(newBaseCurrency);
+    localStorage.setItem(BASE_CURRENCY_KEY, newBaseCurrency);
+  };
+
+  const fetchData = useCallback(
+    async (newBaseCurrency: string) => {
+      if (newBaseCurrency) {
+        await dispatch(await getCurrencies(newBaseCurrency));
+      } else {
+        const savedBaseCurrency = localStorage.getItem(BASE_CURRENCY_KEY);
+        if (savedBaseCurrency) {
+          setBaseCurrency(savedBaseCurrency);
+          await dispatch(await getCurrencies(savedBaseCurrency));
+        } else {
+          const baseCurrencyByLocation = await getLocation();
+          setBaseCurrency(baseCurrencyByLocation);
+          await dispatch(await getCurrencies(baseCurrencyByLocation));
+        }
+      }
+    },
+    [dispatch],
+  );
+
+  useEffect(() => {
+    if (isLoading) {
+      fetchData(baseCurrency).catch(console.error);
+      setIsLoading(false);
+    }
+  }, [baseCurrency, fetchData, isLoading]);
+
+  if (!isLoading) {
+    return (
+      <CurrencyList
+        currencies={currencies_state.currencies}
+        isLoadingState={currencies_state?.loading}
+        baseCurrency={baseCurrency}
+        changeBaseCurrency={changeBaseCurrency}
+      />
+    );
+  } else {
+    return <Loading />;
+  }
 };
 
 export default Currencies;
