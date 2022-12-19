029fca3bbc2369addcdf7f073cbf9ff8ebbaaf2c
Pavarha Maksim <dilexet@gmail.com>
2022-12-19 23:42:05 +0300

    changed _app.tsx

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
+import { store, wrapper } from "../redux/store";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 };
 
-export default App;
+export default wrapper.withRedux(App);
