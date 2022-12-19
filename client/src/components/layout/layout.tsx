097e8f2b93589a028ffba4b5e313b4c3c11fc3a2
Pavarha Maksim <dilexet@gmail.com>
2022-12-19 23:41:54 +0300

    changed layout.tsx

--- client/src/components/layout/layout.tsx
+++ client/src/components/layout/layout.tsx
@@ -1,29 +1,25 @@
-import { Provider } from "react-redux";
 import { Box, CssBaseline } from "@mui/material";
-import store from "../../redux/store";
 import HeaderContainer from "../../containers/header/header-container";
 import FooterContainer from "../../containers/footer/footer-container";
 import { ILayoutProps } from "../../types/layout/layout-props";
 
 const Layout = ({ children }: ILayoutProps) => {
   return (
-    <Provider store={store}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "100vh",
-        }}
-      >
-        <CssBaseline>
-          <HeaderContainer />
-          <main>
-            {children}
-          </main>
-          <FooterContainer />
-        </CssBaseline>
-      </Box>
-    </Provider>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <CssBaseline>
+        <HeaderContainer />
+        <main>
+          {children}
+        </main>
+        <FooterContainer />
+      </CssBaseline>
+    </Box>
   );
 };
 
